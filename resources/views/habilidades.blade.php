6260cd55ba074e15d12ab1512614bd5e3bcfc206
Estudiante UNAB <estudiante@unab.edu.co>
2026-03-26 23:17:00 +0000

    Commit 6: Vista habilidades.blade.php creada

---
+++ resources/views/habilidades.blade.php
@@ -0,0 +1,1 @@
+<!DOCTYPE html><html><head><title>Habilidades</title><link rel="stylesheet" href="{{ asset('css/estilos.css') }}"></head><body><nav><a href="/perfil">Perfil</a> | <a href="/perfil/intereses">Intereses</a> | <a href="/perfil/habilidades">Habilidades</a> | <a href="/perfil/metas">Metas</a></nav><h1>Skills Técnicas</h1><p>Dominio de HTML, CSS básico, y PHP.</p></body></html>
